5cbe1faa6592da69b61d51f6466b622df363eb6a
Vaibhav Sawate <97944040+Vaibhav-Sawate@users.noreply.github.com>
2025-11-28 20:42:08 +0000

    Solved CORS issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-
+const cors = require('cors');
 //Load env variable
 dotenv.config();
 
@@ -11,9 +11,11 @@ connectDB();
 const app = express();
 
 //MIDDLEWARE
+app.use(cors());   //to allow all domain to look and talk to ys
 //allows app to read the json from frontend an postman
 //without this req body is undefined
 app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 
 
 //ROUTES
@@ -22,9 +24,6 @@ app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/expenses', require('./routes/expenseRoutes'));
 
 const PORT = process.env.PORT || 5000;
-
-
-
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port: ${PORT}`);
