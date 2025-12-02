41936d5b16d57532a368956be9950720830a4860
Vaibhav Sawate <97944040+Vaibhav-Sawate@users.noreply.github.com>
2025-12-02 12:24:05 +0000

    Vercel Setting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios';
+
+
+// --- CRITICAL CONFIGURATION ---
+// If Vercel gives us a URL, use it. Otherwise use "" (Proxy).
+const apiUrl = import.meta.env.VITE_API_URL || '';
+
+// This sets the base address for every single axios command in your app
+axios.defaults.baseURL = apiUrl;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
